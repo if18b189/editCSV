90ccf2ed9ac820db6e10aee5f6c646cf4672d57b
if18b189 <49987011+if18b189@users.noreply.github.com>
2021-02-22 04:58:15 +0100

    - drafted some ideas

--- src/main.py
+++ src/main.py
@@ -4,10 +4,16 @@ import os
 import glob
 
 
+class EntryData:
+    def __init__(self):
+        print("test")
+
+    def getWeek(self):
+        print("week")
+
+
 def loadCSV():
     csv = filedialog.askopenfile()
-
-
 
 
 if __name__ == '__main__':
@@ -15,13 +21,25 @@ if __name__ == '__main__':
 
     master.title('CSVEdit')  # title of the program window
 
-    master.geometry("")  # defining the window size
+    master.geometry("")  # defining the window size, "" means window will adjust to the widgets
 
-    for x in range(7):
+    rowFrame = dict()
 
+    for rowNumber in range(9):  # setting up the grid for the calendar entries
+        rowFrame[rowNumber] = tk.Frame(master)
+        rowFrame[rowNumber].pack(side='top')
 
+        for columnNumber in range(8):
+            if rowNumber == 0:
+                Label = tk.Label(rowFrame[rowNumber], text='Hello', bg='white', fg='black', width=10, height=5,
+                                 borderwidth=1, relief="solid")
+                Label.pack(side="left")
+            else:
+                Label = tk.Label(rowFrame[rowNumber], text='Hello', bg='blue', fg='white', width=10, height=5,
+                                 borderwidth=1, relief="solid")
+                Label.pack(side="left")
 
     loadCSVButton = tk.Button(master, text='Load CSV', width=15, height=2, command=loadCSV)
-    loadCSVButton.pack(side="left", padx=10, pady=10)
+    loadCSVButton.pack(side="bottom", padx=10, pady=10)
 
     master.mainloop()
