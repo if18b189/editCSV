536982dc052a1cfacaf8cbef6fd931c4a5eb4bf7
if18b189 <49987011+if18b189@users.noreply.github.com>
2021-02-22 04:06:14 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import os
+import glob
+
+
+def loadCSV():
+    csv = filedialog.askopenfile()
+
+
+if __name__ == '__main__':
+    master = tk.Tk()  # creating a tk application+
+
+    master.title('CSVEdit')  # title of the program window
+
+    master.geometry("")  # defining the window size
+
+    loadCSVButton = tk.Button(master, text='Load CSV', width=15, height=2, command=loadCSV)
+    loadCSVButton.pack(side="left", padx=10, pady=10)
+
+    master.mainloop()
